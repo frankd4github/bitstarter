dc487f36f3f9b52f481d9e8cf76fcac12faa9630
Frank Da Silva <frank.dasilva@gmail.com>
2013-07-06 23:44:44 -0400

    Works with URL

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = 'file://index.html';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -43,6 +46,28 @@ var cheerioHtmlFile = function(htmlfile) {
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
+
+var getURL = function(url, whatToDo) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) throw result;
+	whatToDo(result);
+    });
+}
+
+var checkHtmlURL = function (url, checksfile) {
+    function whatToDo (data) {
+	$ = cheerio.load(data);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for(var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+	}
+	var outJson = JSON.stringify(out, null, 4);
+	console.log(outJson);
+    }
+    getURL(url, whatToDo)
+}
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
@@ -64,11 +89,18 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to index.html', undefined, undefined)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+	console.log('URL provided. Using URL');
+	checkHtmlURL(program.url, program.checks);
+    } else {
+	console.log('URL not provided. Using file ' + program.file)
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
